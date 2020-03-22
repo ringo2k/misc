cd5cf50eb283322b46c819936af6fe9ae6359262
crecht <crecht@rootfrogs.com>
2020-03-22 13:52:24 +0100

    new script for splitting multiple files in subfolders

---
+++ splitInFolders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "usage - give folder in param1, all files in this folder will be split in sperated folder with 20 files each"
+#!/bin/bash
+
+dir_size=20
+dir_name="folder"
+n=$((`find . -maxdepth 1 -type f | wc -l`/$dir_size+1))
+for i in `seq 1 $n`;
+do
+    mkdir -p "$dir_name$i";
+    find . -maxdepth 1 -type f | head -n $dir_size | xargs -i mv "{}" "$dir_name$i"
+done
