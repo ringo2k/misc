3f5f542d18a61134ca65e08ca3f297fcfc129e3d
crecht <crecht@rootfrogs.com>
2020-04-10 23:05:45 +0200

    improved python skeleton

--- skeleton.py
+++ skeleton.py
@@ -1,9 +1,47 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+import sys
 import argparse
 
-parser = argparse.ArgumentParser(description="test")
-parser.add_argument('--foo','-f', action='store_true', help="foo test")
-parser.add_argument('--bar','-b', type=int, help="bar test")
-args = parser.parse_args()
+def main(args = sys.argv[1:]):
+    parser = argparse.ArgumentParser()
 
-print(args)
+    parser.add_argument("--square",
+                        help="Used for testing",
+                        type=int,
+                        default=None)
+
+    parser.add_argument("--int_value",
+                        help="display a square of a given number",
+                        type=int)
+
+    parser.add_argument("--float_value",
+                        help="display a square of a given number",
+                        type=int)
+
+    parser.add_argument("-f",
+                        "--flag",
+                        help="Specify a flag",
+                        action="store_true")
+    parser.add_argument("--rating",
+                        help="An option with a limited range of values",
+                        choices=[1, 2, 3],
+                        type=int)
+
+    # Allow --day and --night options, but not together.
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--day",
+                       help = "mutually exclusive option",
+                       action = "store_true")
+    group.add_argument("--night",
+                       help = "mutually exclusive option",
+                       action = "store_true")
+
+    args = parser.parse_args(args)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
--- splitInFolders.sh
+++ splitInFolders.sh
@@ -3,11 +3,12 @@
 echo "usage - give folder in param1, all files in this folder will be split in sperated folder with 20 files each"
 #!/bin/bash
 
-dir_size=20
+dir_size=200
 dir_name="folder"
 n=$((`find . -maxdepth 1 -type f | wc -l`/$dir_size+1))
 for i in `seq 1 $n`;
 do
     mkdir -p "$dir_name$i";
+    echo "$dir_name$i";
     find . -maxdepth 1 -type f | head -n $dir_size | xargs -i mv "{}" "$dir_name$i"
 done
